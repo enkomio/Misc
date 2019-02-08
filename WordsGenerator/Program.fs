a46cf34b1dd4bf74109d0dea95c27d5712c498cb
Antonio Parata <aparata@gmail.com>
2019-02-09 00:00:07 +0100

    Added WordsGenerator program

---
+++ WordsGenerator/Program.fs
@@ -0,0 +1,18 @@
+﻿let alphabet = "ABCD"
+
+let rec generate(str, len) =
+    alphabet
+    |> Seq.map(fun c ->
+        let newString = str + c.ToString()
+        if newString.Length > len 
+        then [newString] :> string seq
+        else generate(newString, len)
+    )
+    |> Seq.concat
+
+[<EntryPoint>]
+let m _ = 
+    for i=0 to alphabet.Length-1 do
+        generate("", i) 
+        |> Seq.iter(printfn "%s")
+    0
