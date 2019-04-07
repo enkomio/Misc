484efb70bbd94e875bae8e46a0f4753b3aebcb8d
Antonio Parata <aparata@gmail.com>
2019-04-07 15:13:09 +0200

    Added code problem

---
+++ CodeJam2019/Foregone Solution/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace CodeJam2019.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("CodeJam2019")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("CodeJam2019")>]
+[<assembly: AssemblyCopyright("Copyright ©  2019")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("ed81d6d9-037e-4f36-9946-9ffe3c4f4bc8")>]
+
+// Version information for an assembly consists of the following four values:
+//
+//       Major Version
+//       Minor Version
+//       Build Number
+//       Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ CodeJam2019/Foregone Solution/Program.fs
@@ -0,0 +1,43 @@
+﻿open System
+
+
+let rec computeNumbers(inputA: Int32, inputB: Int32) =
+    let mutable a = inputA
+    let mutable b = inputB
+    let mutable exponent = 0
+    let mutable reDo = false
+
+    let aStr = a.ToString().ToCharArray() |> Array.rev
+    let bStr = b.ToString().ToCharArray() |> Array.rev
+
+    for j=0 to aStr.Length-1 do
+        if not reDo then
+            let ac = aStr.[j]
+            let bc = bStr.[j]
+
+            if ac = '4' || bc = '4' then
+                let increment = 1 * (int32 <| Math.Pow(10., float exponent))
+                a <- a + increment
+                b <- b - increment
+                reDo <- true
+
+            exponent <- exponent + 1
+
+    if reDo 
+    then computeNumbers(a, b)
+    else (new String(aStr |> Array.rev), new String(bStr |> Array.rev))
+
+[<EntryPoint>]
+let main argv = 
+    let testCases = Int32.Parse(Console.ReadLine().Trim())
+    for i=0 to testCases-1 do
+        let N = Int32.Parse(Console.ReadLine().Trim())        
+        let mutable a = N / 2
+        let mutable b = a
+
+        if a+b < N then
+            a <- a + 1
+
+        let (aStr, bStr) = computeNumbers(a, b)
+        Console.WriteLine("Case #{0}: {1} {2}", i+1, aStr, bStr)
+    0
